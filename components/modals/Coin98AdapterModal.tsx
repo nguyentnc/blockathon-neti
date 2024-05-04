8e2e4a97390fc805d9d274f7c4c9af2fa1fc7f06
Nguyen Tran <nguyentnc@coin98.finance>
2024-05-04 14:31:34 +0700

    feat: add web3 service

---
+++ components/modals/Coin98AdapterModal.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+import { viction } from '@coin98-com/wallet-adapter-react-ui';
+
+const WalletModalC98 = dynamic(
+  async () => (await import('@coin98-com/wallet-adapter-react-ui')).WalletModalC98,
+  {
+    ssr: false,
+  }
+);
+
+const Coin98AdapterModal = () => {
+  const defaultChains = [viction];
+  return <WalletModalC98 isC98Theme enableChains={defaultChains} />;
+};
+
+export default Coin98AdapterModal;
