21fb9c2ba94e6170b036204e794662fec00edc73
Nguyen Tran <nguyentnc@coin98.finance>
2024-05-05 07:10:20 +0700

    update

--- services/MissionService.tsx
+++ services/MissionService.tsx
@@ -19,10 +19,7 @@ export class MissionService {
   async claimReward(amount: string, claimId: number) {
     const adapterWallet = this.adapter!;
     const rewardContract = this.web3.getContract(REWARD_ABI, REWARD_ADDRESS);
-    console.log(
-      '🚀 ~ file: MissionService.tsx:23 ~ REWARD_ADDRESS:',
-      REWARD_ADDRESS
-    );
+    console.log('🚀 ~ file: MissionService.tsx:23 ~ REWARD_ADDRESS:', REWARD_ADDRESS);
     const signer = new ethers.Wallet(
       '0x967edf8107e48f73b4f02b60f2d08d53a83b0262d0513e99cf30df1f2c4d989d'
     );
@@ -36,7 +33,7 @@ export class MissionService {
     );
 
     const rewardAbi = rewardContract.methods
-      .claimReward({ user: adapterWallet.address, amount: Number(1), claimId: 1 }, signature)
+      .claimReward([adapterWallet.address, amount, claimId], signature)
       .encodeABI();
 
     const tx = {
